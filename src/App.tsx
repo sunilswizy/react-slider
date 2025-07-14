716709efc46bcb797042bd35d09452aee55a54cd
Sunil J <82895610+sunilswizy@users.noreply.github.com>
2025-07-14 05:50:43 +0000

    added assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
 import Slider from './components/slider/slider.component';
+import ja from '../public/locals/translation.json';
 
 
 function App() {
+  console.log("Ja", ja)
 
   return (
     <>
